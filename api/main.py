bbd04394103999e65947d7d5de55f97b1ddca0ca
Daniel Reis <reisdaniel739@gmail.com>
2026-05-17 16:29:03 -0300

    Conectando FastAPI ao PostgreSQL

--- api/main.py
+++ api/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from db import get_connection
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"mensagem": "BookingHub funcionando!"}
+
+@app.get("/testar-banco")
+def testar_banco():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT version();")
+    resultado = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return {"postgresql": resultado[0]}
